8036f6bd0339869dd05eae87d47c2312609e8b48
denischagin <denischaginnn@gmail.com>
2024-05-30 12:37:36 +0300

    feat: сделал свод для сухой

--- src/widgets/dryWoodVaultStats/ui/DryWoodVaultStats.tsx
+++ src/widgets/dryWoodVaultStats/ui/DryWoodVaultStats.tsx
@@ -1,12 +1,64 @@
-import { Box } from '@mui/material'
+import { FC, useMemo } from 'react'
+
+import { skipToken } from '@reduxjs/toolkit/query'
+
+import { Box, Skeleton } from '@mui/material'
 
 import { VaultItem } from '@/entities/vault'
+import { useFetchWoodArrivalByRangeQuery } from '@/entities/wood-arrival'
+import { useFetchAllWoodConditionsQuery } from '@/entities/wood-condition'
+import { useFetchWoodShipmentByRangeQuery } from '@/entities/wood-shipment'
 
-export const DryWoodVaultStats = () => {
+export type DryWoodVaultStatsProps = {
+  startDate: string
+  endDate: string
+}
+
+export const DryWoodVaultStats: FC<DryWoodVaultStatsProps> = ({ startDate, endDate }) => {
+  const { data: woodConditions, isLoading: isLoadingWoodConditions } =
+    useFetchAllWoodConditionsQuery()
+
+  const dryWoodCondition = useMemo(() => {
+    return woodConditions?.find(condition => condition.name === 'Сухая')
+  }, [woodConditions])
+
+  const { data: woodArrivalByRange, isLoading: isLoadingWoodArrival } =
+    useFetchWoodArrivalByRangeQuery(
+      dryWoodCondition
+        ? {
+            startDate,
+            endDate,
+            woodConditionId: dryWoodCondition.id,
+          }
+        : skipToken
+    )
+  const { data: woodShipmentByRange, isLoading: isLoadingWoodShipment } =
+    useFetchWoodShipmentByRangeQuery(
+      dryWoodCondition
+        ? {
+            startDate,
+            endDate,
+            woodConditionId: dryWoodCondition.id,
+          }
+        : skipToken
+    )
+
+  const isLoading = isLoadingWoodArrival || isLoadingWoodConditions || isLoadingWoodShipment
   return (
-    <Box display='flex' gap={3} mt={5} flexWrap='wrap'>
-      <VaultItem title='Поступило' />
-      <VaultItem title='Отгрузили' />
+    <Box display='flex' gap={3} mt={5} flexWrap='wrap' justifyContent='space-evenly'>
+      {isLoading && (
+        <>
+          <Skeleton variant='circular' width='700px' height='700px' />
+          <Skeleton variant='circular' width='700px' height='700px' />
+          <Skeleton variant='circular' width='700px' height='700px' />
+        </>
+      )}
+      {woodArrivalByRange && (
+        <VaultItem title='Поступило' sunburstData={woodArrivalByRange.sunburstData} />
+      )}
+      {woodShipmentByRange && (
+        <VaultItem title='Отгрузили' sunburstData={woodShipmentByRange.sunburstData} />
+      )}
     </Box>
   )
 }
