147f2f41639ecdbda3d6a404bdade1d4611d0046
iklimov <respondatanytime@gmail.com>
2024-06-01 15:53:59 +0300

    feat: Добавить переход на один день в цехе при двойном нажатии на строку в сводной таблице

--- src/entities/workshop/ui/WorkshopTotalTable.tsx
+++ src/entities/workshop/ui/WorkshopTotalTable.tsx
@@ -1,9 +1,9 @@
 import { FC, useMemo } from 'react'
 
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { Box, CircularProgress } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, GridEventListener } from '@mui/x-data-grid'
 
 import { useFetchWorkshopReportQuery } from '@/entities/workshop-out'
 import { TimeRange } from '@/shared/types'
@@ -16,6 +16,7 @@ import {
 
 import { WORKSHOP_TOTAL_TABLE_COLUMNS } from '../constants'
 import dayjs from 'dayjs'
+import { urls } from '@/shared/constants'
 
 type WorkshopTotalTableProps = {
   timeRange: TimeRange
@@ -23,6 +24,7 @@ type WorkshopTotalTableProps = {
 
 export const WorkshopTotalTable: FC<WorkshopTotalTableProps> = ({ timeRange }) => {
   const { workshopId } = useParams()
+  const navigate = useNavigate()
 
   const { data: workshopOutReportData, isLoading: isLoadingWorkshopOutReport } =
     useFetchWorkshopReportQuery({
@@ -40,6 +42,10 @@ export const WorkshopTotalTable: FC<WorkshopTotalTableProps> = ({ timeRange }) =
       return { ...reportItem, date: dayjs(reportItem.date).format('DD.MM.YYYY') }
     })
   }, [workshopOutReportData])
+
+  const handleRowDoubleClick: GridEventListener<'rowDoubleClick'> = event => {
+    navigate(`/${urls.workshop}/${workshopId}/${urls.day}?date=${event.row.date}`)
+  }
 
   return (
     <DataGridContainer
@@ -64,6 +70,7 @@ export const WorkshopTotalTable: FC<WorkshopTotalTableProps> = ({ timeRange }) =
           sx={{ ...dataGridStyles, width: 400 }}
           hideFooter
           slots={{ panel: CustomGridPanel }}
+          onRowDoubleClick={handleRowDoubleClick}
         />
       )}
     </DataGridContainer>
