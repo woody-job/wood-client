55635f2362e3e8bb279e2c4162f2b962ec9cd5b9
ilya_the_creator <respondatanytime@gmail.com>
2024-07-24 11:17:48 +0300

    ST-3-112 - Удаление количества и диаметра из склада сырья

--- src/entities/beam-warehouse/constants/constants.ts
+++ src/entities/beam-warehouse/constants/constants.ts
@@ -1,8 +1,6 @@
 import { GridColDef } from '@mui/x-data-grid'
 
 export const BEAM_WAREHOUSE_TABLE_COLUMNS: GridColDef[] = [
-  { headerName: 'Условное обозначение', field: 'woodNaming', flex: 1 },
-  { headerName: 'Диаметр', field: 'diameter', flex: 1 },
-  { headerName: 'Кол-во', field: 'amount', flex: 1 },
-  { headerName: 'Объем, м3', field: 'volume', flex: 1 },
+  { headerName: 'Условное обозначение', field: 'woodNaming', width: 200 },
+  { headerName: 'Объем, м3', field: 'volume', width: 200 },
 ]
